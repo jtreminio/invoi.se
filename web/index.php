111780166a3ee2c39f86f20b6f5c2945614238b4
Juan Treminio <jtreminio@gmail.com>
2013-04-23 19:19:30 -0500

    Added ConfigServiceProvider

--- web/index.php
+++ web/index.php
@@ -1,12 +1,6 @@
 <?php
+
 require_once __DIR__.'/../vendor/autoload.php';
-
-require __DIR__ . '/../src/Container.php';
-
-$container = new Container;
-
-require __DIR__ . '/../src/Invoise/Controllers.php';
-
-$app = $container->getApp();
+require_once __DIR__.'/../src/App.php';
 
 $app->run();
